99a144c81b58b3cadc5594cc07feddcf608c844c
Hugo Josefson <hugo@josefson.org>
2023-02-11 22:37:05 +0100

    test: add failing call test with params

--- connection_test.ts
+++ connection_test.ts
@@ -53,3 +53,54 @@ Deno.test("call", async () => {
 
   assertEquals(res, { jsonrpc: "2.0", id: 1, result: "hoge" });
 });
+
+Deno.test("call with params", async () => {
+  const p1 = new Buffer();
+  const p2 = new Buffer();
+  const serverStream = new VSCodeStream(p1, p2);
+  const clientStream = new VSCodeStream(p2, p1);
+
+  const h = (mes: RequestMessage): Promise<ResponseMessage> => {
+    if (mes.method === "testWithParams") {
+      const res: ResponseMessage = {
+        jsonrpc: "2.0",
+        id: mes.id,
+        result: `params: ${JSON.stringify(mes.params)}`,
+      };
+      return Promise.resolve(res);
+    }
+
+    const res: ResponseMessage = {
+      jsonrpc: "2.0",
+      id: mes.id,
+      error: {
+        code: -32601,
+        message: "not found",
+      },
+    };
+    return Promise.resolve(res);
+  };
+
+  const nh = (_mes: NotificationMessage): Promise<void> => Promise.resolve();
+
+  const serverConn = new Connection(serverStream, { handle: h }, {
+    handle: nh,
+  });
+  const clientConn = new Connection(clientStream, { handle: h }, {
+    handle: nh,
+  });
+
+  serverConn.listen();
+  clientConn.listen();
+
+  const res = await clientConn.call("testWithParams", "hoge", "fuga");
+
+  clientConn.close();
+  serverConn.close();
+
+  assertEquals(res, {
+    jsonrpc: "2.0",
+    id: 1,
+    result: `params: ["hoge","fuga"]`,
+  });
+});
